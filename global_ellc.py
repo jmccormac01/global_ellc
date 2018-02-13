33fd26219fe83e896b47932921078bbf4c006c7a
James McCormac <jmccormac001@gmail.com>
2018-02-13 22:38:43 +0000

    removes large docstring as info now in readme

--- global_ellc.py
+++ global_ellc.py
@@ -1,77 +1,7 @@
 """
 A generalised multi-instrument version of James Blake's emcee_combined.py
 
-This code was written to model eclipsing MS+BD binaries and so contains
-flexibility on a subset of binary parameters. More flexability can be
-included easily later.
-
-To run the code you must specify a configuration file which describes the
-data location, mcmc setup and initial parameters + priors etc. See below
-for the example config file for J234318.41 (eclipsing BD):
-
-Lines starting with # are ignored (comments). The comments mingled with
-the configurations below explain how things are set up.
-
-###############################
-######  DATA DESCRIPTION ######
-###############################
-# location
-data_dir /Users/jmcc/Dropbox/EBLMs/J23431841
-out_dir /Users/jmcc/Dropbox/EBLMs/J23431841/output
-# data files, formats expected are either:
-# lc filter filename
-# rv instrument filename
-lc Clear NITES_J234318.41_Clear_20120829_F1_A14.lc.txt
-lc Clear NITES_J234318.41_Clear_20130923_F2_A14.lc.txt
-lc Clear NITES_J234318.41_Clear_20131010_F1_A14.lc.txt
-lc Clear NITES_J234318.41_Clear_20141001_F1_A14.lc.txt
-rv FIES J234318.41_NOT.rv
-rv SOPHIE J234318.41_SOPHIE.rv
-rv PARAS J234318.41_PARAS.rv
-###############################
-######  MCMC DESCRIPTION ######
-###############################
-# number of MCMC steps
-nsteps 10
-# scale MCMC nwalkers
-# nwalkers = walker_scaling * 4*n_parameters
-walker_scaling 1
-# MCMC parameters can be either:
-#   Fixed parameters:
-#        parameter_name F value
-#   Uniform prior parameters:
-#       parameter_name U seed_value weight prior_l prior_h
-#   No prior parameters:
-#       parameter_name N seed_value weight
-# walkers are normally distributed around (seed_value, weights)
-sbratio F 0.0
-r1_a U 0.029363 0.0001 0.02 0.04
-r2_a U 0.004665 0.0001 0.002 0.007
-incl U 89.6232 0.01 88.0 90.0
-t0 N 2453592.74192 0.001
-period N 16.9535452 0.0001
-ecc U 0.16035 0.001 0.1 0.2
-omega U 78.39513 0.1 70.0 90.0
-a_r1 U 31.650747 0.1 28.0 36.0
-ldc_1_1 F 0.3897
-ldc_1_2 F 0.1477
-q U 0.09649 0.001 0.05 0.145
-# systemic velocity parameters are instrument specific
-# they are best kept to the end of the config file and defined as:
-#   vsys U instrument seed_value weight prior_l prior_h
-vsys U FIES -21.133 0.1 -25.0 -15.0
-vsys U SOPHIE -21.122 0.1 -25.0 -15.0
-vsys U PARAS -20.896 0.1 -25.0 -15.0
-
-The config file will be read and all parameters are stored in the
-'config' object. Different numbers of instruments and filters
-should be handled automatically.
-
-The following output is made:
-    - plot of each variable parameter's walker
-    - corner plot of the post-burnin chain segment
-    - plot of data with fitted model
-    - output of the best fitting parameters
+See the repository README.md for instructions
 
 Contributors:
     James McCormac + James Blake
